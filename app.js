dae4d21e5df68cd63f12538ef066806247b86e37
Corise <contact@thecorise.com>
2025-01-15 01:59:01 +0500

    cors updated

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors({
+  origin: ["http://45.133.178.107:3000/"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 const app = express();
 app.use(cookieParser());

7af805f7bbbbba6cb8963bec58c87d9071c7e10b
Corise <contact@thecorise.com>
2025-01-15 21:25:10 +0500

    update authToken cookie issue

--- routers/auth.js
+++ routers/auth.js
@@ -79,7 +79,7 @@ router.post("/api/login", (req, res) => {
             checkUser.generateAuthToken()
               .then((token) => {
                 res.cookie("authToken", token, {
-                  httpOnly: false,
+                  httpOnly: true,
                   secure: true, 
                   sameSite: "none",
                   maxAge: 24 * 60 * 60 * 1000, 

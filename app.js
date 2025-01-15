17b9d012c95f5c8863cf90afbe1472a7986bb21c
Corise <contact@thecorise.com>
2025-01-15 21:06:19 +0500

    udpate cors issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 const app = express();
 const corsOptions = {
